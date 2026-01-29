9fdf816163bbfc16e3a12ec56485969f2ef1ae9d
Siri <siri@Siris-MacBook-Air.local>
2026-01-28 18:18:32 -0600

    folder structure created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating the directory structure
+mkdir -p src
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+touch steup.py
+
+
+echo "Directory and files created successfully"
