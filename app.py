167e15abdfe7d22def37f7f95a24bb377699cefa
Saikesana31 <kesana.class2024@gmail.com>
2026-01-30 18:30:08 -0600

    final update

--- app.py
+++ app.py
@@ -68,13 +68,6 @@ def chat():
 
 
 
-
-
-
-
-
-
-
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)  
--- src/template.py
+++ src/template.py
@@ -1,6 +1,6 @@
 # Prompt template
 template = """
-    You are a medical expert.can u act as medical assistant and answer the question based on the documents.
+    You are a medical expert.can u act as medical assistant and answer the question in a friendly and engaging way.
     You are given a question and a retrived context to answer the question.
     If u dont know the answer, just say "I don't know"
     
