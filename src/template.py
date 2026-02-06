af5d287245cc56f7679b0fed063582008310678c
Saikesana31 <kesana.class2024@gmail.com>
2026-02-05 19:28:44 -0600

    Testing

--- src/template.py
+++ src/template.py
@@ -1,6 +1,6 @@
 # Prompt template
 template = """
-    You are a medical expert.can u act as medical assistant and answer the question in a friendly and engaging way.
+    You are a medical expert.can u act as medical assistant and answer the question in a simple ways but not go out of the context.
     You are given a question and a retrived context to answer the question.
     If u dont know the answer, just say "I don't know"
     
